a43a0fd8f6cc8f8b25fadb9d089cdd1945947ce9
sahenke13 <shenke13@gmail.com>
2018-10-04 13:38:49 -0500

    added Geo Code

---
+++ geo.js
@@ -0,0 +1,35 @@
+      //google maps location code.  Provides lat and long 
+      var map, infoWindow;
+      function initMap() {
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            console.log(pos)
+            //create lat and long variables to pass on to eventful 
+            var locLat = pos.lat;
+            var locLng = pos.lng;
+            console.log("Latitude: "+locLat);
+            console.log("Longitude: " + locLng);
+
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
---
+++ steven.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+
+
+//googleMaps ApiKey
+
+googleApiKey = "AIzaSyCyzG2it5G1mxi_GPoa85F-ol0GdWx4rXs"; 
+
+
+function googleAjaxRequest(){
+
+    //need to set up the queryUrl
+
+
+    queryUrl="";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(googleResponse){
+
+    var dataMaps = googleResponse.data;
+
+
+
+
+
+
+
+})
+
+
+
+}
+
+
+
+});
